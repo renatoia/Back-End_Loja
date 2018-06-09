133b61b371e59755a4a56687a731d3abcbbe6180
renatoia <renatoadsfatecfranca@gmail.com>
2018-06-08 23:35:13 -0300

    Parte 1 - Atualização

--- controllers/contact.js
+++ controllers/contact.js
@@ -6,7 +6,7 @@ module.exports = function() {
         
         var fs = require('fs');
 
-        fs.writeFile("../arquivo/contato.txt", JSON.stringify(req.body), {flag: "a"}, function(erro) {
+        fs.writeFile("C:/6not/back-end/arquivo/contato.txt", JSON.stringify(req.body), {flag: "a"}, function(erro) {
 
             if(erro) {
                 throw erro;
